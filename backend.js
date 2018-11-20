b184139729f43c7e47cd45f40040e8be6b838c11
Saba Schock <sababorjsaba@gmail.com>
2018-11-19 18:28:09 -0800

    version 0.0.2

--- backend.js
+++ backend.js
@@ -1,59 +1,71 @@
-
 $(document).ready(function () {
-    $("#search").on("click", function (event) {
+    $(document).on("click", "#search", function () {
+        // clean up the result section from previous results
         event.preventDefault();
-        var searchTxt = $("#search-term").val();
-        var numberRec = $("#number-records").val();
-        var startYear = $("#start-year").val();
-        var endYear = $("#end-year").val();
-        console.log(searchTxt, numberRec, startYear, endYear);
-    })
-    $("#reset").on("click", function (event) {
-        event.preventDefault();
-        var searchTxt = $("#search-term").val(null);
-        var numberRec = $("#number-records").val(null);
-        var startYear = $("#start-year").val(null);
-        var endYear = $("#end-year").val(null);
-        console.log(searchTxt, numberRec, startYear, endYear);
-    })
-})
-$(document).on("click", "#search", function () {
-    event.preventDefault();
-    var limitNum = parseInt($("#number-records").val());
-    console.log(limitNum);
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-        'api-key': "315b4cda26814b54ad0934919f414cb2",
-        'q': $("#search-term").val(),
-        'begin_date': parseInt($("#start-year").val() + "0101"),
-        'end_date': parseInt($("#end-year").val() + "0101"),
-        'sort': "newest"
+        $("#results").empty();
+        $("#resultHeader").empty();
+        // store user input 
+        var searchKey = $("#search-term").val();
+        var startD = $("#start-year").val();
+        var endID = $("#end-year").val();
+        var limitNum = parseInt($("#number-records").val());
+
+        //create the ajax url
+        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        url += '?' + $.param({
+            'api-key': "315b4cda26814b54ad0934919f414cb2",
+            'q': $("#search-term").val(),
+            'begin_date': parseInt($("#start-year").val() + "0101"),
+            'end_date': parseInt($("#end-year").val() + "0101"),
+            'sort': "newest"
+        });
+
+        // use ajax method to call NYT API
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function (result) {
+
+            // function to get reslt from the response and put them in the result section
+            function getResult(number) {
+                // fishing values from the response JSON object
+                for (var i = 0; i < number; i++) {
+                    var doc = result.response.docs[i];
+                    var authorData = doc.byline.original;
+                    var titleData = doc.headline.main;
+                    var dateData = doc.pub_date;;
+                    var linkData = doc.web_url;
+                    console.log(author, title, date, link);
+
+                    // dynamically creating html element for the values using jQuery
+                    var title = $(`<div id="title"><strong>${titleData}</strong></div>`);
+                    var author = $(`<div id="Author">${authorData}</div>`);
+                    var date = $(`<div id="date">${dateData}</div>`);
+                    var link = $(`<a id="link" href="${linkData}">${linkData}</a><br><hr>`);
+
+                    // appending the results to html
+                    $("#results").append(title, author, date, link)
+                }
+            }
+
+            var responseCount = result.response.docs.length
+
+            // if enough articles has been found
+            if (responseCount >= limitNum) {
+                $("#resultHeader").append(`Here is ${limitNum} articles reagrding ${searchKey} from ${startD} to ${endID}:`)
+                getResult(limitNum);
+            }
+
+            // if less or no articles has been found
+            else if (responseCount < limitNum) {
+                console.log(responseCount);
+                $("#resultHeader").append(`${responseCount} articles reagrding ${searchKey} from ${startD} to ${endID} has been found!`)
+                getResult(responseCount);
+            }
+
+        // catch the error
+        }).fail(function (err) {
+            throw err;
+        });
     });
-    $.ajax({
-        url: url,
-        method: 'GET',
-    }).done(function (result) {
-        console.log(result)
-        // $("#results").empty();
-        for (var i = 0; i < limitNum; i++) {
-            console.log("here")
-            var doc = result.response.docs[i];
-            var authorData = doc.byline.original;
-            var titleData = doc.headline.main;
-            var dateData = doc.pub_date;;
-            var linkData = doc.web_url;
-            console.log(author,title,date,link);
-     
-            var title = $(`<div id="title"><strong>${titleData}</strong></div>`);
-            var author = $(`<div id="Author">${authorData}</div>`);
-            var date = $(`<div id="date">${dateData}</div>`);
-            var link = $(`<a id="link" href="${linkData}">${linkData}</a><br><hr>`);
-
-             $("#results").append(title,author,date,link)
-
-        }
-    }).fail(function (err) {
-        throw err;
-    });
-
 })
